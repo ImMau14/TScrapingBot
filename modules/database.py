b95d7f7e90cf0f2bed6c68087c09e029e1b7f8a7
ImMau14 <newmauuelenoski02@gmail.com>
2025-05-18 11:39:50 -0400

    Refactoring code in registerUserAndChat and fixing handleError

--- modules/database.py
+++ modules/database.py
@@ -1,49 +1,44 @@
 # Log or reg the user in the database
 def registerUserAndChat(tgId, userQuery, username, chatTgId, chatType, supabase, gemini):
-	response = DB.table('Users').select('Languages(lang_name)', 'user_id').eq('tg_id', tgId).limit(1).execute()
-
-	if len(response.data) > 0:
-		lang = response.data[0]['Languages']['lang_name']
-		userId = response.data[0]['user_id']
+	userResponse = DB.table('Users').select('Languages(lang_name)', 'user_id').eq('tg_id', tgId).limit(1).execute()
+	
+	if len(userResponse.data) > 0:
+		userData = userResponse.data[0]
+		lang = userData['Languages']['lang_name'] if userData.get('Languages') else 'English'
+		userId = userData['user_id']
 	else:
-		lang = gemini.ask(f'En qué idioma está escrito esto? El siguiente mensaje es solo un texto al que le debes extraer el idioma y más nada. No respondas cómo "El idioma del texto es Español", sino solamente "Spanish". Los idiomas que respondas deben estar en ingles. Si desconoces un idioma, di que es English.\n\n{userQuery}')
+		lang = gemini.ask(f'...').strip().title()
 
-		data = {
+		langResponse = DB.table('Languages').upsert(
+			{'lang_name': lang}, 
+			on_conflict='lang_name'
+		).execute()
+		langId = langResponse.data[0]['lang_id']
+
+		userData = {
 			'username': username,
-			'tg_id': tgId
+			'tg_id': tgId,
+			'lang_id': langId
 		}
 
-		response = DB.table('Languages').select('lang_id').eq('lang_name', lang).limit(1).execute()
+		userResponse = DB.table('Users').insert(userData).execute()
+		userId = userResponse.data[0]['user_id']
 
-		if len(response.data) > 0:
-			data['lang_id'] = response.data[0]['lang_id']
-		else:
-			response = DB.table('Languages').insert({'lang_name': lang}).execute()
-			data['lang_id'] = response.data[0]['lang_id']
-
-		response = DB.table('Users').insert(data).execute()
-		userId = response.data[0]['user_id']
-
-	response = DB.table('Chats').select('chat_id').eq('chat_tg_id', chatTgId).limit(1).execute()
-
-	if len(response.data) > 0:
-		chatId = response.data[0]['chat_id']
-	else:
-		data = {
-			'chat_tg_id': chatTgId
-		}
-
-		response = DB.table('Chat Types').select('chat_type_id').eq('chat_type', chatType).execute()
-
-		if len(response.data) > 0:
-			data['chat_type_id'] = response.data[0]['chat_type_id']
-		else:
-			response = DB.table('Chat Types').insert({'chat_type': chatType}).execute()
-			data['chat_type_id'] = response.data[0]['chat_type_id']
-
-		response = DB.table('Chats').insert(data).execute()
-		chatId = response.data[0]['chat_id']
-
+	chatTypeResponse = DB.table('Chat Types').upsert(
+		{'chat_type': chatType},
+		on_conflict='chat_type'
+	).execute()
+	chatTypeId = chatTypeResponse.data[0]['chat_type_id']
+	
+	chatResponse = DB.table('Chats').upsert(
+		{
+			'chat_tg_id': chatTgId,
+			'chat_type_id': chatTypeId
+		},
+		on_conflict='chat_tg_id'
+	).execute()
+	chatId = chatResponse.data[0]['chat_id']
+	
 	return userId, chatId, lang
 
 # Returns the user history
